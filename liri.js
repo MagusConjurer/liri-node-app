6ce55299dc5acbbfa375c4c4c0c872dfd0b993b3
MagusConjurer <cameronisadavis@gmail.com>
2020-04-28 21:17:53 -0600

    Installed npm packages and setup base function of concert-this

--- liri.js
+++ liri.js
@@ -1,13 +1,32 @@
 require("dotenv").config();
+var axios = require("axios");
+var fs = require("fs");
 var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
-
-var bandURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
-
+var moment = require("moment");
+//var spotify = new Spotify(keys.spotify);
 
 // Use Bands in Town to return a concert for the artist
-function concertThis(){
-
+// node liri.js concert-this <artist/band name here>
+function concertThis(artist){
+  var bandURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+  axios.get(bandURL)
+  .then(function(response){
+    var venue = response.data[0].venue.name;
+    var location = response.data[0].venue.location;
+    // Use moment to convert to MM/DD/YYYY
+    var date = response.data[0].datetime;
+    console.log(venue, location, date);
+  }).catch(function(error){
+    if(error.response){
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if(error.request){
+      console.log(error.request);
+    } else {
+      console.log("Error", error.message);
+    };
+  });
 };
 
 // Use Spotify to identify the artist and provide a preview
@@ -29,3 +48,10 @@ function doWhatItSays(){
 function logAction(){
 
 }
+
+var action = process.argv[2];
+var input = process.argv[3];
+
+if(action = "concert-this"){
+  concertThis(input);
+};
